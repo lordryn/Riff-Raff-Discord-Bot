836751d81f8013b1f09d386bb095ef76a0792026
lordryn <69993664+lordryn@users.noreply.github.com>
2022-05-20 04:02:37 +0000

    bonghit set

--- main.py
+++ main.py
@@ -116,9 +116,11 @@ async def on_message(message):
     # todo add suggestions command
     if user_message.lower().split(' ')[0] == '!bonghit':
         ma_id = '<@542436163395387407>'
-        emoji = get(message.server.emojis, name="BongHit")#'977020269682106419'
+        emoji = client.get_emoji(977020269682106419)
         await message.add_reaction(emoji)
-        await message.chanel.send(f'{ma_id} {username} has redeemed a bong hit!')
+        await message.channel.send(f'{ma_id} {username} has redeemed a bong hit!')
+        print(f'{username}->bonghit ')
+        
 
     # converts post command text to rswiki link
     if user_message.lower().split(' ')[0] == '!rswiki':
